0ca760424eff33bd03cbfecd7f5961295de4372b
Sergey Mochalov <proweb@users.noreply.github.com>
2022-10-06 17:56:04 +0300

    main.js fixes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-import "../node_modules/uikit/dist/css/uikit.min.css";
-
+import "../node_modules/uikit/src/less/uikit.less";
 import UIkit from "uikit";
 import Icons from "uikit/dist/js/uikit-icons";
 
